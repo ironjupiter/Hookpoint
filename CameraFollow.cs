b519c3becbccbdc21432f47087d4e856202c1c05
aaranpl <142637400+aaranpl@users.noreply.github.com>
2023-08-20 09:25:30 -0700

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private void LateUpdate(){
+        transform.position = target.position;
+    }
+}
